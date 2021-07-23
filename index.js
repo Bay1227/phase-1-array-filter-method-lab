3da1ef3db3dfbf1dca1dc49f4d2ca790d52639c8
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-07-22 19:04:16 -0700

    Completed Filter Lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter (el => el.charAt(0).toUpperCase() + el.slice(1) === name);
+}
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter (name => name.startsWith(letters));
+}
+function  matchName(drivers, match) {
+    return drivers.filter(obj => obj.name === match);
+}
